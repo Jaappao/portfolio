a13a28257e3086484ed4721405da2edd87bbe991
Hideki NIHOMMATSU <tech.hidewagon2000@gmail.com>
2022-12-08 21:47:20 +0900

    assetPrefix updated

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   basePath: process.env.GITHUB_ACTIONS && "/portfolio",
-  assetPrefix: process.env.GITHUB_ACTIONS && "/portfolio",
+  assetPrefix: process.env.GITHUB_ACTIONS ? "/portfolio/" : '/',
     webpack: function (config) {
         config.module.rules.push(
           {
